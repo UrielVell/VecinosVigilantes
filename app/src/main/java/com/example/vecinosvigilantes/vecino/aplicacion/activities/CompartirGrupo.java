0baba4df364e44ef45ec9e1ab893d01b43283636
David Vega Tepetla <pescador2111@hotmail.com>
2023-04-24 16:31:55 -0600

    pantalla compartir grupo agregada

---
+++ app/src/main/java/com/example/vecinosvigilantes/vecino/aplicacion/activities/CompartirGrupo.java
@@ -0,0 +1,17 @@
+package com.example.vecinosvigilantes.vecino.aplicacion.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vecinosvigilantes.R;
+
+public class CompartirGrupo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compartir_grupo);
+
+    }
+}
