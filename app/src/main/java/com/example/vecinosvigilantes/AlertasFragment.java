b4716e98a83a024e124173fa2ee1978688389393
Oscar <pereag3o@gmail.com>
2023-05-18 17:16:05 -0600

    mostrar alerta

--- app/src/main/java/com/example/vecinosvigilantes/AlertasFragment.java
+++ app/src/main/java/com/example/vecinosvigilantes/AlertasFragment.java
@@ -1,5 +1,7 @@
 package com.example.vecinosvigilantes;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,17 +35,13 @@ public class AlertasFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+    / * @param param1 Parameter 1.
+    / * @param param2 Parameter 2.
      * @return A new instance of fragment AlertasFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AlertasFragment newInstance(String param1, String param2) {
+    public static AlertasFragment newInstance() {
         AlertasFragment fragment = new AlertasFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -53,12 +52,57 @@ public class AlertasFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alertas2, container, false);
+        // alerta
+        View root = inflater.inflate(R.layout.fragment_alertas2, container, false);
+        ImageButton alertaVecino = root.findViewById(R.id.imageButton);
+        ImageButton alertaSospechoso = root.findViewById(R.id.imageButton2);
+        alertaVecino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            mostrarDialogoAlerta();
+            }
+        });
+        alertaSospechoso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarDialogoSospechoso();
+            }
+        });
+
+        return root;
     }
+
+    private void mostrarDialogoAlerta(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Alerta");
+        builder.setMessage("Estan asaltando a un vecino, urge ir a ayudarlo")
+                .setPositiveButton("Enterado", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).setCancelable(false)
+                .show();
+
+    }
+    private void mostrarDialogoSospechoso(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Alerta");
+        builder.setMessage("Hay un sospechoso en la calle, estar al pendiente por cualquier cosa")
+                .setPositiveButton("Enterado", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).setCancelable(false)
+                .show();
+    }
+
+
 }
